d6f1628ecedb718645fbf12620252391889abd72
MatthewKonen <58831422+MatthewKonen@users.noreply.github.com>
2022-01-26 20:53:13 -0800

    Add files via upload

---
+++ bakery-web-page-CSI340/Models/Ingredients.cs
@@ -0,0 +1,11 @@
+﻿namespace bakery_web_page_CSI340.Models
+{
+    public class Ingredients
+    {
+        public int IngredientsId { get; set; }
+        public string RecipieId { get; set; }
+        public string IngredientsName { get; set; }
+        public string IngredientsType { get; set; }
+        public int IngredientsQuantity { get; set; }
+    }
+}
---
+++ bakery-web-page-CSI340/Models/ItemIngredients.cs
@@ -0,0 +1,16 @@
+﻿namespace bakery_web_page_CSI340.Models
+{
+    public class ItemIngredients
+    {
+        public int Id { get; set; }
+
+        public int MenuItemID { get; set; }
+
+        public int IngredientIDs { get; set; }
+
+        public MenuItems MenuItems { get; set; }
+
+        public Ingredients Ingredients { get; set; }
+
+    }
+}
